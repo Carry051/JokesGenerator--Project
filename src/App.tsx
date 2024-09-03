387e5615f4dd89f68d96b808c43d3eac05985b20
Maksym <mvoron@gmail.com>
2024-09-03 10:32:08 +0200

    isDone

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,26 @@ import axios from 'axios';
 const App = () => {
     const [joke, setJoke] = useState([]);
 
-    const getJoke = async () => {
-        const res = await axios.get('https://icanhazdadjoke.com/', {
-            headers: { Accept: 'application/json' },
-        });
-        setJoke(res.data.joke);
+    const getJoke = () => {
+        axios
+            .get('https://icanhazdadjoke.com/', {
+                headers: { Accept: 'application/json' },
+            })
+            .then((res) => setJoke(res.data.joke))
+            .catch((err) => console.error(err));
     };
 
     return (
         <div className='text-4xl text-white flex flex-col h-screen items-center justify-center gap-10'>
-            <h1 className='text-7xl mb-10'>Jokes Generator</h1>
-            <div className='h-10 mb-10 '>
-                <p>{joke.length === 0 ? `Click the button` : joke}</p>
+            <h1 className='text-7xl flex gap-6'>
+                <p className='first-letter:text-green-500 '>Jokes</p>
+                <p className='first-letter:text-green-500'>Generator</p>
+            </h1>
+            <div className='h-10 my-16 max-w-[1000px] text-center'>
+                <p>{joke.length === 0 ? <p>Click the button...</p> : joke}</p>
             </div>
             <button
-                className='text-white border-2 p-4 hover:text-black hover:bg-white rounded-lg'
+                className='text-white border-2 p-4  hover:bg-green-500 rounded-lg'
                 onClick={getJoke}
             >
                 Get Joke
